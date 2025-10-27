a76126f9ee76f8a93e8a5887f52ce490223b44bc
Sine Uyeno <Jude.Uyeno@edu.sait.ca>
2025-10-27 08:19:13 -0600

    Part 2 complete

--- app/App.jsx
+++ app/App.jsx
@@ -7,36 +7,18 @@
 import React from 'react';
 import {
   Button,
-  Pressable,
   SafeAreaView,
-  ScrollView,
   StyleSheet,
-  Text,
   TextInput,
   View
 } from 'react-native';
+import ToDoList from './ToDoList';
 
 
-function App() {
+export function App() {
   return (
     <SafeAreaView>
-      <ScrollView>
-        <Pressable>
-          <View style={[styles.task, styles.completed]}>
-            <Text style={styles.taskText}>Do laundry</Text>
-          </View>
-        </Pressable>
-        <Pressable>
-          <View style={[styles.task]}>
-            <Text style={styles.taskText}>Go to gym</Text>
-          </View>
-        </Pressable>
-        <Pressable>
-          <View style={[styles.task, styles.completed]}>
-            <Text style={styles.taskText}>Walk dog</Text>
-          </View>
-        </Pressable>
-      </ScrollView>
+      <ToDoList/>
       <View style={styles.form}>
         <TextInput
           style={styles.input}
@@ -49,17 +31,6 @@ function App() {
 }
 
 const styles = StyleSheet.create({
-  task: {
-    padding: 10,
-    borderBottomWidth: 1,
-    borderColor: '#ccc',
-  },
-  completed: {
-    backgroundColor: '#e0e0e0',
-  },
-  taskText: {
-    fontSize: 16,
-  },
   form: {
     flexDirection: 'row',
     justifyContent: 'space-between',
