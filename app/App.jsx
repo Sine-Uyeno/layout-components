c398ccb87a0cb8cd7c0794da7217c9045e3a63fd
Sine Uyeno <Jude.Uyeno@edu.sait.ca>
2025-10-27 08:23:07 -0600

    Part 2 & 3 Finished

--- app/App.jsx
+++ app/App.jsx
@@ -6,12 +6,9 @@
 
 import React from 'react';
 import {
-  Button,
-  SafeAreaView,
-  StyleSheet,
-  TextInput,
-  View
+  SafeAreaView
 } from 'react-native';
+import ToDoForm from './ToDoForm';
 import ToDoList from './ToDoList';
 
 
@@ -19,33 +16,11 @@ export function App() {
   return (
     <SafeAreaView>
       <ToDoList/>
-      <View style={styles.form}>
-        <TextInput
-          style={styles.input}
-          placeholder="Add a new task..."
-        />
-        <Button title="Add" />
-      </View>
+      <ToDoForm/>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  form: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginHorizontal: 20,
-    marginTop: 20,
-  },
-  input: {
-    flex: 1,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    marginRight: 10,
-  },
-});
+
 
 export default App;
